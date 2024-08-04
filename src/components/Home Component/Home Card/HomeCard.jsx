b43f6f8f3ccbd0a93eeafc4860e54c8e5aaf0b19
Anmol Jha <anmoljha098@gmail.com>
2024-08-04 17:37:08 +0530

    Update HomeCard

--- src/components/Home Component/Home Card/HomeCard.jsx
+++ src/components/Home Component/Home Card/HomeCard.jsx
@@ -1,7 +1,9 @@
+import "./HomeCard.css";
+
 function HomeCard({ username, src, text }) {
   return (
     <>
-      <div className="relative h-[800px] w-[600px] ml-10">
+      <div className="home__card relative h-[800px] w-[600px] ml-10">
         <img src={src} className="z-0 h-full w-full object-cover" />
         <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
         <div className="absolute bottom-4 left-4 text-left">
